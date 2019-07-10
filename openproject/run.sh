15fdf02f38f04b305edfedfb81f0e74c0026af38
wmyaoyao <wmyaoyao@gmail.com>
2019-07-10 09:21:01 +0000

    update openproject to v9

--- openproject/run.sh
+++ openproject/run.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+# Reference: 
+# https://www.openproject.org/docker/
+# https://github.com/opf/openproject/blob/dev/docs/configuration/configuration.md
+PORT=8080
+SECRET_KEY=secret
+SMTP_PASS="<apply at sendgrit>"
+SMTP_DOMAIN=example.org
+
+
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 VOL_HOST=$DIR/volumes
 
 # See: https://www.openproject.org/docker/
-mkdir -p $VOL_HOST/{pgdata,logs,static}
+mkdir -p $VOL_HOST/{pgdata,static}
 
 echo "# Launching openproject"
 docker rm -fv openproject
-docker run -d -p 8080:80 --name openproject -e SECRET_KEY_BASE=secret \
-  -v $VOL_HOST/pgdata:/var/lib/postgresql/9.4/main \
-  -v $VOL_HOST/logs:/var/log/supervisor \
-  -v $VOL_HOST/static:/var/db/openproject \
-  openproject/community:7
+docker run -d -p $PORT:80 --name openproject \
+  -e SECRET_KEY_BASE=$SECRET_KEY \
+  -e EMAIL_DELIVERY_METHOD=smtp \
+  -e SMTP_ADDRESS=smtp.sendgrid.net \
+  -e SMTP_PORT=587 \
+  -e SMTP_DOMAIN=$SMTP_DOMAIN \
+  -e SMTP_AUTHENTICATION=login \
+  -e SMTP_ENABLE_STARTTLS_AUTO=true \
+  -e SMTP_USER_NAME="apikey" \
+  -e SMTP_PASSWORD=$SMTP_PASS \
+  -v $VOL_HOST/pgdata:/var/openproject/pgdata \
+  -v $VOL_HOST/static:/var/openproject/assets \
+  openproject/community:9
+
+# FIXME: delete logs volume??
+#  -v $VOL_HOST/logs:/var/log/supervisor \
 
 echo "# Should be ok now, please open:"
 echo "http://localhost:8080  (Default login = admin/admin) "
