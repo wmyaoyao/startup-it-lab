21c40f14a61607d8961ca4c68b0a6de662b25376
William Yao <wmyaoyao@gmail.com>
2020-09-25 15:21:17 +0800

    use local docker volume to prevent mount permission error

--- doccano/run.sh
+++ doccano/run.sh
@@ -2,10 +2,13 @@
 # Note: Check if the docker image is up to date
 # https://hub.docker.com/r/doccano/doccano/builds
 
-VOLUME=$PWD/volume
-mkdir -p $VOLUME
+#VOLUME=$PWD/volume
+#mkdir -p $VOLUME
+
+VOLUME=doccano-data
 echo "VOLUME=$VOLUME"
 
+docker rm -f doccano
 docker run -d --name doccano \
   -e "ADMIN_USERNAME=admin" \
   -e "ADMIN_EMAIL=admin@example.com" \
