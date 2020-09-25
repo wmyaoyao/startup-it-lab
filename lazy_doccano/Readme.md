1e8ba8be2abeacb21bf891b87530ac34577669a6
William Yao <wmyaoyao@gmail.com>
2020-09-25 14:41:49 +0800

    add doccano

---
+++ lazy_doccano/Readme.md
@@ -0,0 +1,7 @@
+Please refer to the original repo at : https://github.com/doccano/doccano/
+
+An all-in-one docker is available at:
+https://hub.docker.com/r/doccano/doccano/builds
+
+The script here provides a simple way to launch the service for 
+non-production scenarios.
---
+++ lazy_doccano/run.sh
@@ -0,0 +1,15 @@
+
+# Note: Check if the docker image is up to date
+# https://hub.docker.com/r/doccano/doccano/builds
+
+VOLUME=$PWD/volume
+mkdir -p $VOLUME
+echo "VOLUME=$VOLUME"
+
+docker run -d --name doccano \
+  -e "ADMIN_USERNAME=admin" \
+  -e "ADMIN_EMAIL=admin@example.com" \
+  -e "ADMIN_PASSWORD=password" \
+  -v $VOLUME:/data \
+  -p 8000:8000 doccano/doccano
+
