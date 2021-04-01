63f4f2e1a5b2b7dcbd32f1f361bffa8a4a59fa7c
William Yao <wmyaoyao@gmail.com>
2021-04-01 17:35:43 +0800

    run.sh

--- bitwarden/run.sh
+++ bitwarden/run.sh
@@ -6,4 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 VOLUME=$DIR/bw-data
 echo "VOLUME=$VOLUME"
 
-docker run -d --name bitwarden -v $VOLUME:/data/ -p 80:80 bitwardenrs/server:latest
+# You may want to change this password...
+ADMIN_PAGE_PASS=bwadmin
+
+docker rm -fv bitwarden
+docker run -d --name bitwarden \
+	-v $VOLUME:/data/ \
+	-p 8090:80 \
+	-e ADMIN_TOKEN=$ADMIN_PAGE_PASS \
+       bitwardenrs/server:latest
+
+echo ""
+echo "# NOTE: "
+echo "# Admin page: http://<server IP>:8090/admin"
+echo "# pass = $ADMIN_PAGE_PASS"
